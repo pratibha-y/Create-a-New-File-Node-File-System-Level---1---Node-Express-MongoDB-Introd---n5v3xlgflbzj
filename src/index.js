923e07030fb95c89bd5093c6c854d120159f6873
Pratibha-y <yadavpratibha501@gmail.com>
2023-04-30 18:42:22 +0530

    update index file

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
+  await file.writeFile(fileName,content);
   // write code here
   // dont change function name
+  
+
 };
 
 module.exports = { writeFile };
